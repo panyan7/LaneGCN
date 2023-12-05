7f304b6f3a55ae1765bafad34538dcf4ba211a7c
panyan7 <ypan2@andrew.cmu.edu>
2023-12-05 05:10:20 +0000

    Modify get_data.sh

--- get_data.sh
+++ get_data.sh
@@ -2,7 +2,7 @@
 mkdir dataset && cd dataset
 
 # step1: download Argoverse HD Maps
-wget https://s3.amazonaws.com/argoai-argoverse/hd_maps.tar.gz
+wget "https://s3.amazonaws.com/argoverse/datasets/av1/tars/hd_maps.tar.gz"
 tar xf hd_maps.tar.gz
 # copy map to argoverseapi root folder
 PY_SITE_PACKAGE_PATH=$(python -c 'import site; print(site.getsitepackages()[0])')
@@ -11,12 +11,12 @@ cp -r map_files $PY_SITE_PACKAGE_PATH
 
 # step2: download Argoverse Motion Forecasting **v1.1** 
 # train + val + test
-wget https://s3.amazonaws.com/argoai-argoverse/forecasting_train_v1.1.tar.gz
-wget https://s3.amazonaws.com/argoai-argoverse/forecasting_val_v1.1.tar.gz
-wget https://s3.amazonaws.com/argoai-argoverse/forecasting_test_v1.1.tar.gz
-tar xvf forecasting_train_v1.1.tar.gz
-tar xvf forecasting_val_v1.1.tar.gz
-tar xvf forecasting_test_v1.1.tar.gz
+wget "https://s3.amazonaws.com/argoverse/datasets/av1/tars/forecasting_train.tar.gz"
+wget "https://s3.amazonaws.com/argoverse/datasets/av1/tars/forecasting_val.tar.gz"
+wget "https://s3.amazonaws.com/argoverse/datasets/av1/tars/forecasting_test.tar.gz"
+tar xvf forecasting_train.tar.gz
+tar xvf forecasting_val.tar.gz
+tar xvf forecasting_test.tar.gz
 
 # step3: preprocess data to accelerate training
 
@@ -24,6 +24,6 @@ tar xvf forecasting_test_v1.1.tar.gz
 python preprocess_data.py -m lanegcn
 
 # STEP3-Option2: Download from my digitalocean space
-# wget https://yun.sfo2.cdn.digitaloceanspaces.com/public/lanegcn/test_test.p
-# wget https://yun.sfo2.cdn.digitaloceanspaces.com/public/lanegcn/train_crs_dist6_angle90.p
-# wget https://yun.sfo2.cdn.digitaloceanspaces.com/public/lanegcn/val_crs_dist6_angle90.p
+# wget"https://yun.sfo2.cdn.digitaloceanspaces.com/public/lanegcn/test_test.p
+# wget"https://yun.sfo2.cdn.digitaloceanspaces.com/public/lanegcn/train_crs_dist6_angle90.p
+# wget"https://yun.sfo2.cdn.digitaloceanspaces.com/public/lanegcn/val_crs_dist6_angle90.p
